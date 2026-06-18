8f485932df4fb1abeefd321e89129602a68a3c40
AnNguyen-Druig <nguyentrandatan@gmail.com>
2026-06-18 21:57:05 +0700

    feat(course/initial-add-profile-service): integrate profile-service with KeyCloak

--- profile-service/src/main/java/com/devteria/profile/service/ProfileService.java
+++ profile-service/src/main/java/com/devteria/profile/service/ProfileService.java
@@ -1,16 +1,25 @@
 package com.devteria.profile.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.devteria.profile.dto.identity.Credential;
+import com.devteria.profile.dto.identity.TokenExchangeParam;
+import com.devteria.profile.dto.identity.UserCreationParam;
 import com.devteria.profile.dto.request.RegistrationRequest;
 import com.devteria.profile.dto.response.ProfileResponse;
 import com.devteria.profile.mapper.ProfileMapper;
+import com.devteria.profile.repository.IdentityClient;
 import com.devteria.profile.repository.ProfileRepository;
+
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @Slf4j
@@ -19,16 +28,64 @@ import java.util.List;
 public class ProfileService {
     ProfileRepository profileRepository;
     ProfileMapper profileMapper;
+    IdentityClient identityClient;
 
-    public List<ProfileResponse> getAllProfiles(){
+    @Value("${idp.client-id}")
+    @NonFinal
+    String clientId;
+
+    @Value("${idp.client-secret}")
+    @NonFinal
+    String clientSecret;
+
+    public List<ProfileResponse> getAllProfiles() {
         var profiles = profileRepository.findAll();
         return profiles.stream().map(profileMapper::toProfileResponse).toList();
     }
 
-    public ProfileResponse register(RegistrationRequest request){
+    public ProfileResponse register(RegistrationRequest request) {
+        // Create account in KeyCloak
+        // Exchange client token
+        var token = identityClient.exchangeToken(TokenExchangeParam.builder()
+                .grant_type("client_credentials")
+                .client_id(clientId)
+                .client_secret(clientSecret)
+                .scope("openid")
+                .build());
+
+        log.info("TokenInfo: {}", token);
+
+        // Create user with client Token and given info
+        var creationResponse = identityClient.createUser(
+                "Bearer " + token.getAccessToken(),
+                UserCreationParam.builder()
+                        .username(request.getUsername())
+                        .firstName(request.getFirstName())
+                        .lastName(request.getLastName())
+                        .email(request.getEmail())
+                        .enabled(true)
+                        .emailVerified(false)
+                        .credentials(List.of(Credential.builder()
+                                .type("password")
+                                .temporary(false)
+                                .value(request.getPassword())
+                                .build()))
+                        .build());
+
+        // Get userId of KeyCloak account
+        String userId = extractUserId(creationResponse);
+        log.info("UserId: {}", userId);
+
         var profile = profileMapper.toProfile(request);
+        profile.setUserId(userId);
         profile = profileRepository.save(profile);
 
         return profileMapper.toProfileResponse(profile);
+    }
+
+    private String extractUserId(ResponseEntity<?> response) {
+        String location = response.getHeaders().get("Location").getFirst();
+        String[] splitStr = location.split("/");
+        return splitStr[splitStr.length - 1];
     }
 }
